d2aa1159836fdba7b75bd5b102cc31cf36b7ad0c
Ryuck24 <dalejandroortiz05@gmail.com>
2023-09-30 16:56:50 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "";
+    $years = 0;
+
+    echo "Bienvenido Ingresa tu nombre \n";
+    $name = readline();
+
+    echo "Bienvenido" . $name "\n";
+
+    echo "Ingresa tu edad: \n";
+    $years = readline();
+
+
+?>  
